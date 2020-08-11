d6687b733cc4121d1d8759d8f4349d3050ae0fa6
Nipun Sampath <nipunsampath@gmail.com>
2020-08-11 13:14:08 +0530

    implemented save  and get employee methods

--- server.js
+++ server.js
@@ -1,66 +1,83 @@
 const express = require("express");
 const axios = require("axios");
 const redis = require("redis");
-
+const cors = require('cors');
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 // setup redis client
-
 const client = redis.createClient(
   process.env.REDIS_PORT,
   process.env.REDIS_HOST, {
   auth_pass: process.env.REDIS_PASSWORD,
-  tls: { servername: process.env.REDIS_HOST },
+  // tls: { servername: process.env.REDIS_HOST },
 }
 );
 
 // redis store configs
 const employeeRedisKey = "store:employees"; // cahce key for employeees
-const dataExpireTime = 300; // 1 hour cache expire time
+const dataExpireTime = process.env.CACHE_PERIOD; // 1 hour cache expire time
 
 // main endpoint
 app.get("/", (req, res) =>
   res.send("Welcome to Node.js + redis boilerplate API.")
 );
 
-// employee endpoint with caching
-app.get("/employees", (req, res) => {
-  // try to fetch the result from redis
-  return client.get(employeeRedisKey, (err, employees) => {
-    if (employees) {
-      return res.json({ source: "cache", data: JSON.parse(employees) });
+// get employee endpoint with caching
+app.get("/employees", async (req, res) => {
+  return client.get(employeeRedisKey, async (err, employees) => {
+    console.log("here");
+    if (!err) {
+      if (employees) {
+        // try to fetch the result from redis
+        return res.json({ source: "cache", data: JSON.parse(employees) });
+      } else {
+        // get data from remote API
+        const res = await axios.get(process.env.GET_EMPLOYEE_ENDPOINT)
+          .then((res) => res.data)
+          .then((employees) => {
+            // save the API response in redis store
+            client.setex(employeeRedisKey, dataExpireTime, JSON.stringify(employees));
 
-      // if cache not available call API
-    } else {
-      // get data from remote API
-      axios
-        .get("https://employee-main.azurewebsites.net/employees")
-        .then((res) => res.data)
-        .then((users) => {
-          // save the API response in redis store
-          client.setex(employeeRedisKey, 300, JSON.stringify(users));
-
-          // send JSON response to client
-          return res.json({ source: "api", data: users });
-        })
-        .catch((error) => {
-          // send error to the client
-          return res.json(error.toString());
-        });
+            // send JSON response to client
+            return res.json({ source: "api", data: employees });
+          })
+          .catch((error) => {
+            // send error to the client
+            return res.json(error.toString());
+          });
+      }
     }
+    else {
+      console.log(err.toString());
+    }
+
   });
 });
 
-// user details endpoint
-app.get("/", (req, res) =>
-  res.send("Welcome to Node.js + Redis boilerplate API.")
-);
+// save user endpoint
+app.post("/saveEmployee", async (req, res) => {
+  const properties = { headers: { "Content-Type": "application/json" } };
+
+  axios.post(process.env.SAVE_EMPLOYEE_ENDPOINT, req.body, properties)
+    .then((result) => {
+
+      if (result.status === 200)
+        res.sendStatus(200);
+      else
+        res.sendStatus(500);
+    });
+});
+
+
 
 // start express server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log("Server listening on port:", PORT);
 });
